24ca014a1d49c908a287d22d1f7dce0b875a5dae
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2025-01-21 11:37:47 +0100

    Revert "Remove redundant stdlib include on Android"
    This reverts commit f1214b9288833dd523f3937b7a4be69d20717370.

--- src/reallocarray.c
+++ src/reallocarray.c
@@ -18,9 +18,8 @@
 // --------
 
 /*
- * Modified version intended for compilation with lower
- * versions of Android's bionic libc (e. g. inside Termux),
- * which don't come with a reallocarray implementation.
+ * Modified version intended only for compilation inside Termux
+ * (which uses Android API 24, which doesn't have reallocarray).
  */
 
 // --------
@@ -30,8 +29,6 @@
 // --------
 
 #if defined __ANDROID_API__ && __ANDROID_API__ <= 29
-
-#include <stdlib.h>
 
 #include <errno.h>
 
--- src/reallocarray.h
+++ src/reallocarray.h
@@ -2,15 +2,13 @@
 #define REALLOCARRAY_H
 // --------
 
+#include <stdlib.h>
+
+// --------
+
 #if defined __ANDROID_API__ && __ANDROID_API__ <= 29
 
 void* reallocarray(void* ptr, size_t nmemb, size_t size);
-
-#else
-
-// Ensure that including this header always
-// provides a `reallocarray` declaration:
-#include <stdlib.h>
 
 #endif
 
